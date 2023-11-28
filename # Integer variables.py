d8b9e02b481b5ddf40d355fa12ec369774a00626
geethikasaisree <geethika.skit@gmail.com>
2023-11-28 20:44:01 +0530

    integer.py

---
+++ # Integer variables.py
@@ -0,0 +1,15 @@
+# Integer variables
+num1 = float(input("enter first number: "))
+num2 = float(input("enter second number: "))
+print(num1, num2)
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(-100)
+print("Absolute Value:", result3)
